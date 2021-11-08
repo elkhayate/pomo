23ee5aee291854d7704ddf3292169d8d78e6a2be
elkhayate <melkhayate61@gmail.com>
2021-11-08 22:45:56 +0200

    logic plan

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 
 import Timer from "./components/Timer";
 function App() {
+
+  // handleWhichValue : switch case which to render to correct value to timer
+  // check value of which then switch background color
   return (
     <div className="App">
       <Timer Minutes = {1} />
--- src/contexts/ContextTime.js
+++ src/contexts/ContextTime.js
@@ -4,6 +4,10 @@ export const ContextTime = createContext();
 
 export default function ContextTimeProvider(props) {
     const [data, setData] = useState(0);
+    //handleData : pomo,long,short values from nav
+    //handleWhich : clicked mode from timer
+    //handleSpeed : calculate speed from switch case state.which then value of selected mode sent to nav
+    // Timer : which
     const handleData = (val) => {
         setData(val)
     }
